9100c01596b245dd48d2064bc23b2e398fabef5e
rache11ee <rlee205@ucr.edu>
2024-02-29 09:55:10 -0800

    some pseudo code for the Enemy class!
    working through the logic

--- CakeCrusade/Enemy.cpp
+++ CakeCrusade/Enemy.cpp
@@ -1,15 +1,25 @@
+//psudo-ish code for enemy class
 #include "Enemy.hpp"
 
-Enemy::Enemy(float h, float dmg, float def) : Entity(h, dmg, def) {
+Enemy::Enemy(float h, float dmg, float def, float spd) : Entity(h, dmg, def, spd) {
     // Constructor implementation can initialize any additional members here
+    : Health(h), Damage(dmg), Defense(def) {}
 }
 
-void Enemy::attackMove() {
+void Enemy::attackMove(player) {
     // Implement how the enemy attacks
+    player.getPosition();
+    enemyPosition = getPosition();
+    playerPosition - enemyPosition;
+
 }
 
 void Enemy::getKnockedBack() {
     // Implement the logic for getting knocked back
+    sf::Vector2f currentPosition = sprite.getPosition();
+    knockbackDirection =
+        sf::Vector2f newPosition = sf::Vector2f(currentPosition.x + knockbackDirection, currentPosition.y)
+        sprite.setPosition(newPosition);
 }
 
 void Enemy::target() {
