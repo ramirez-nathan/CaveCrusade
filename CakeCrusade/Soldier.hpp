bb1e05e8aad1b460ea423ac5a4a37287541ff604
Nathan Ramirez <nater0826@gmail.com>
2024-03-02 00:41:11 -0800

    Removed redundant methods from soldier that are now implemented in Entity

--- CakeCrusade/Soldier.hpp
+++ CakeCrusade/Soldier.hpp
@@ -8,25 +8,9 @@ using namespace std;
 
 class Soldier : public Entity
 {
-private:
-	sf::Vector2f Direction;
-
-
-	int SpriteX = 0;
-	int SpriteY = 0;
-
-	float SoldierSpeed;
-
 public:
 	Soldier(float h, float dmg, float def, float spd);
-	~Soldier();
-
-
-	void initialize();
-	void load();
-	void update(double deltaTime, const sf::Vector2f& target, int level[]);
-	void handleMovement(double deltaTime, sf::Vector2f& movement, int& spriteX, int& spriteY, int level[], vector<int>& walls);
-	void drawSoldier(sf::RenderWindow& window);
+	~Soldier() = default;
 	virtual void attackMove() override;
 
 };
