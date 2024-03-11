340c75397962f53f0e31ac3f70908bf3c6d2d177
Ampherost <Ampherost235@gmail.com>
2024-03-10 19:56:15 -0700

    removed unused variable

--- CakeCrusade/Entity.hpp
+++ CakeCrusade/Entity.hpp
@@ -32,7 +32,6 @@ protected:
     
     sf::RectangleShape BoundingRectangle;
     vector<int> Walls{ 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
-    sf::Vector2f lastAttackDirection; // Vector representing the direction of the last attack
     const float knockbackDistance = 50.0f; // Distance to knock back
 
     
