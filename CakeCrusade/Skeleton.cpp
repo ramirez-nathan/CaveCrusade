8fa18369a370a61ecc377112010ffb441fc733e1
Ampherost <Ampherost235@gmail.com>
2024-03-11 22:55:44 -0700

    skeleton animation pt1

--- CakeCrusade/Skeleton.cpp
+++ CakeCrusade/Skeleton.cpp
@@ -5,7 +5,19 @@ Skeleton::Skeleton(float h, float dmg, float def, float spd)
 
 void Skeleton::load()
 {
-    loadTexture("assets/enemies/skelly/idle/skull_idle.png");
+    // Load the idle texture
+    if (!Texture.loadFromFile("assets/enemies/skelly/idle/skull_idle.png")) {
+        cout << "Cannot load idle texture" << endl;
+    }
+    else {
+        // If loading succeeds, set the sprite to use the idle texture initially
+        Sprite.setTexture(Texture);
+    }
+
+    // Load the attacking texture
+    if (!AttackTexture.loadFromFile("assets/enemies/skelly/attacking/skull_attacking.png")) {
+        cout << "Cannot load attacking texture" << endl;
+    }
     Entity::load();
 }
 
@@ -107,5 +119,30 @@ void Skeleton::draw(sf::RenderWindow& window) {
 
 void Skeleton::attackAnimation(const sf::Vector2f& playerPosition)
 {
-
+    if (ShootingArrow) {
+        if (EntityDirection == 0) { // Looking Down, Looking Down Diagonally
+            AttackingSpriteY = 0;
+        }
+        else if (EntityDirection == 3) { // Looking Right, Looking Right Diagonally
+            AttackingSpriteY = 2;
+        }
+        else if (EntityDirection == 2) { // Looking Left, Looking Left Diagonally
+            AttackingSpriteY = 3;
+        }
+        else if (EntityDirection == 1) { // Looking Up, Looking Up Diagonally
+            AttackingSpriteY = 1;
+        }
+        FinishedBowAnimation = false;
+        if (AttackClock.getElapsedTime().asSeconds() > 0.15f) {
+            if (AttackingSpriteX == 3) { // do nothing, just set shootingspritex up for the next time player shoots 
+                FinishedBowAnimation = true;
+                ShootingArrow = false;
+                AttackingSpriteX = 0; // set it to next frame 
+            }
+            else {
+                AttackingSpriteX++; // set shootingspritex to 0 for first frame
+            }
+            AttackClock.restart();
+        }
+    }
 }
